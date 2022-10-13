62328e163aef2dafffadb2be5d456c3fcf667d0f
othAs-dev <othmane.developer@gmail.com>
2022-10-13 10:26:34 +0200

    lancement du projet

--- unitix_dashboard/src/pages/Home/Home.js
+++ unitix_dashboard/src/pages/Home/Home.js
@@ -1,12 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./Home.css";
 export default function Home() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
-    <div className="w-50">
-      <p className="h3">
-        Bonjour, bienvenue sur le dashboard Unitix. Je vous laisse découvrir les
-        différentes composantes de cet outil
-      </p>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading p-2">Login</h1>
+        <div className="position-relative">
+          <div className="d-flex justify-content-end">
+            <i className="bi bi-person position-absolute"></i>
+          </div>
+
+          <input
+            type="text"
+            placeholder="Username"
+            className="joinInput "
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="position-relative">
+          <div className="d-flex justify-content-end">
+            <i className="bi bi-lock lock position-absolute"></i>
+          </div>
+          <input
+            type="password"
+            placeholder="Password"
+            className="joinInput mt-20"
+            onChange={(e) => setRoom(e.target.value)}
+          />
+        </div>
+        <button className="button mt-20" type="submit">
+          Login
+        </button>
+      </div>
     </div>
   );
 }
